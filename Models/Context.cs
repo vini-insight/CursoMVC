d6e72db0f157a1768355defcd3f2724462411a5f
vini-insight <wynny.eu@gmail.com>
2021-02-28 23:20:07 -0300

    contexto do EF core
    configurando o contexto do EF core para o BD e inicializando o servico no conteiner de injeção de dependencia da classe Startup.cs
    
    001 - Desenvolvimento de aplicações com .NETcore

--- Models/Context.cs
+++ Models/Context.cs
@@ -1,7 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+
 namespace Models
 {
     public class Context : DbContext
     {
-        
+        public DbSet<Categoria> Categorias { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySql("Server=localhost;Port=3306;Database=csharp;Uid=root;Pwd=root;charset=utf8;");
+        }
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Models;
 
 namespace CursoMVC
 {
@@ -24,6 +25,7 @@ namespace CursoMVC
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<Context>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
